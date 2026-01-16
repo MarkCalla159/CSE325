596a88e4fde0df217bcc5123d3cf30038f58687c
Mark Calla <markcalla@byui.edu>
2026-01-16 17:03:41 -0500

    c#

---
+++ PersonalAssignments/w01Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World!");
+Console.WriteLine("Congratulations!");
+Console.WriteLine("You wrote your first lines of code.");
+// Second Exercise
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
